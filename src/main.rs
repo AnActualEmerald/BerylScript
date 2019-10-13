b30288b593754a97b5cf3ea681b85c7e00db5a0d
AnActualEmerald <kevinliger13@gmail.com>
2019-10-12 22:04:22 -0400

    Added file loading and created example file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod lexer;
 
-fn main() {
+use std::fs;
+use std::env;
 
-    let tokens = lexer::tokenize("fn main {print \"hello world\"}");
-    println!("{{print \"hello world\"}}");
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+
+    let data = load_source_file(&args[1]);
+    let tokens = lexer::tokenize(&data);
     println!("{:?}", tokens);
+}
+
+
+fn load_source_file(path: &str) -> String {
+    let res = fs::read_to_string(path).expect(&format!("Had a problem reading the file at {}", path));
+    res
 }
