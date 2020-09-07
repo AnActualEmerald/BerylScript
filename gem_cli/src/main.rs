b41f7897786e20a467863e643eea6e2450c2fabd
AnActualEmerald <kevinliger13@gmail.com>
2020-09-06 22:10:56 -0400

    Now uses clap and can run scripts using a subcommand

--- gem_cli/src/main.rs
+++ gem_cli/src/main.rs
@@ -1,14 +1,30 @@
-// use gem;
-use std::env;
 use std::fs;
 
+// use clap::{App, Arg, SubCommand};
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    let mut args = env::args();
-    // println!("got args: {:?}", args);
-    let file = args.nth(1).unwrap_or_else(|| "".to_owned());
-    // println!("Path to read: {}", file);
-    let data = fs::read_to_string(&file).unwrap_or_else(|e| {
-        panic!("Couldn't read file {}: {}", file, e);
-    });
-    gem::run(data);
+    let matches = clap_app!(app =>
+    (name: "Gem CLI")
+    (version: "0.0.2")
+    (author: "Emerald <@Emerald#6666>")
+    (about: "Runs emerald script programs and other helpful stuff")
+    (@subcommand examples =>
+        (about: "Generates some example files")
+        (@arg PATH: "Where to generate the files, defaults to current directory"))
+    (@subcommand run =>
+        (about: "Run an emerald script program")
+        (@arg PATH: +required "Path of file to run"))
+    )
+    .get_matches();
+
+    if let Some(sub) = matches.subcommand_matches("run") {
+        let path = sub.value_of("PATH").expect("Path should not be NONE");
+        let data = fs::read_to_string(&path).unwrap_or_else(|e| {
+            panic!("Couldn't read file {}: {}", path, e);
+        });
+        gem::run(data);
+    }
 }
