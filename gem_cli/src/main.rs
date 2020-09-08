1c509effe88982f4d88327cfb26fbd5aac1c5bf6
AnActualEmerald <kevinliger13@gmail.com>
2020-09-07 22:45:04 -0400

    Added example file generation

--- gem_cli/src/main.rs
+++ gem_cli/src/main.rs
@@ -1,4 +1,6 @@
+use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 // use clap::{App, Arg, SubCommand};
 
@@ -26,5 +28,42 @@ fn main() {
             panic!("Couldn't read file {}: {}", path, e);
         });
         gem::run(data);
+    }
+
+    if let Some(sub) = matches.subcommand_matches("examples") {
+        let path = if let Some(tmp) = sub.value_of("PATH") {
+            PathBuf::from(tmp)
+        } else {
+            let tmp = env::current_dir().expect("Couldn't get current directory");
+            PathBuf::from(format!("{}{}", tmp.display(), "/examples/"))
+        };
+
+        create_examples(&path);
+    }
+
+    //if there aren't any subcommands we should start a REPL, though that will require modifying how the gem
+    //works as of v0.3
+}
+
+fn create_examples(path: &PathBuf) {
+    let examples = [
+        "fn main() {\n\tprint \"Hello world!\"; \n}",
+        "fn main() {\n\tfor(i = 0; i < 10; i = i + 1){\n\t\tprint \"EmeraldScript rules!\";\n\t}\n}",
+    ];
+
+    println!("Generating example files at {}", path.display());
+
+    //check if the directory exists first, create it if not
+    if let Err(_) = fs::read_dir(&path) {
+        fs::create_dir_all(&path)
+            .expect(format!("Unable to create target directory {}", path.display()).as_str());
+    }
+
+    let mut count = 1;
+    for ex in examples.iter() {
+        let expath = path.join(format!("example{}.em", count));
+        fs::write(&expath, ex)
+            .expect(format!("Error generating example file {}", expath.display()).as_str());
+        count += 1;
     }
 }
