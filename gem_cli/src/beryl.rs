bb07d25420a38605404a1b86be4096954132dedb
AnActualEmerald <kevingauthier1697@gmail.com>
2020-09-20 04:05:51 -0400

    Improved REPL usability

--- gem_cli/src/beryl.rs
+++ gem_cli/src/beryl.rs
@@ -1,34 +1,38 @@
+use console::Term;
 use directories::ProjectDirs;
 use gem::interpreter::*;
 use gem::{lexer, parser};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-use std::fs;
 use std::path::PathBuf;
 
 pub struct Repl {
     runtime: Runtime,
     glob_frame: StackFrame,
     multiline: Vec<bool>,
-    cache: String,
+    cache: PathBuf,
 }
 
 impl Repl {
     pub fn new() -> Self {
-        let cache = ProjectDirs::from("", "EmeraldScript", "Beryl")
-            .expect("Unable to find cache directory");
+        let mut cache = PathBuf::from(
+            ProjectDirs::from("", "EmeraldScript", "Beryl")
+                .expect("Unable to find cache directory")
+                .cache_dir(),
+        );
+        cache.set_file_name("history.txt");
         Repl {
             runtime: Runtime::new(),
             glob_frame: StackFrame::new(),
             multiline: vec![],
-            cache: format!("{}/{}", cache.cache_dir().to_str().unwrap(), "history.txt"),
+            cache: cache,
         }
     }
 
     pub fn run(&mut self, debug: bool) {
         let mut rl = Editor::<()>::new();
         if rl.load_history(&self.cache).is_err() {
-            fs::create_dir_all(PathBuf::from(&self.cache)).expect("Unable to create history file");
+            println!("Couldn't load history");
         }
 
         println!(
@@ -37,25 +41,77 @@ impl Repl {
         );
         println!("do \"gem --help\" to see other commands");
         println!("Type exit or stop to leave\n");
-
+        let mut prompt = "<== ".to_owned();
+        let mut data = String::new();
         loop {
-            let input = rl.readline("---> ");
+            let input = rl.readline(&prompt);
             match input {
-                Ok(line) => {
-                    //do repl stuf here
-                    if line.ends_with("{\n") {
-                        self.multiline.push(true);
-                    } else if line.ends_with("}\n") {
-                        self.multiline.pop();
+                Ok(raw) => {
+                    data.push_str(&raw);
+                    match raw.as_str() {
+                        "exit" | "stop" => break,
+                        "clear" => {
+                            let buf = Term::stdout();
+                            buf.clear_screen().expect("Unable to clear console");
+                            println!(
+                                "Welcome to Beryl, the interactive EmeraldScript interpreter v{}!",
+                                env!("CARGO_PKG_VERSION")
+                            );
+                            println!("do \"gem --help\" to see other commands");
+                            println!("Type exit or stop to leave\n");
+                        }
+
+                        line => {
+                            //do repl stuf here
+                            if line.ends_with("{") {
+                                self.multiline.push(true);
+                            } else if line.ends_with("}") {
+                                self.multiline.pop();
+                            }
+
+                            if self.multiline.contains(&true) {
+                                let mut tmp = "... ".to_owned();
+                                for _ in self.multiline.iter() {
+                                    tmp.push('\t');
+                                }
+                                prompt = tmp;
+                                rl.add_history_entry(line);
+                                continue;
+                            } else {
+                                prompt = "<== ".to_owned();
+                            }
+
+                            let tokens = lexer::run(&data);
+                            data = String::new();
+                            if debug {
+                                println!("Generated tokens: {:?}", tokens);
+                            }
+                            match parser::parse(tokens) {
+                                Ok(ast) => {
+                                    if debug {
+                                        println!("Generated AST: {:?}", ast);
+                                    }
+
+                                    println!("");
+                                    match repl_run(ast, &mut self.runtime, &mut self.glob_frame) {
+                                        Ok(_) => {}
+                                        Err(e) => println!("{}", e),
+                                    }
+                                    println!("");
+                                }
+                                Err(e) => println!("{}", e),
+                            }
+
+                            rl.add_history_entry(line);
+                        }
                     }
-                    rl.add_history_entry(line.as_str());
-                    println!("Line: {}", line);
                 }
-                Err(ReadlineError::Interrupted) => {
-                    println!("CTRL-C");
+                Err(ReadlineError::Eof) => break,
+                Err(ReadlineError::Interrupted) => break,
+                Err(err) => {
+                    println!("{}", err.to_string());
                     break;
                 }
-                _ => println!("{:?}", input),
             }
 
             rl.save_history(&self.cache).unwrap();
