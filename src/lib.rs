a4a6905ea0f219c4e2969434a4eda4fac077917a
AnActualEmerald <kevinliger13@gmail.com>
2021-05-27 03:19:55 -0400

    Changing to a LALRPOP parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,13 @@
 #![feature(test)]
 
-pub mod interpreter;
-pub mod lexer;
-pub mod parser;
+#[macro_use]
+extern crate lalrpop_util;
 
-#[cfg(test)]
-mod bench;
+pub mod parser;
 
 ///Runs the lexer, parser, and interpreter on the provided string
 pub fn run(data: String, args: &str, debug: bool) {
-    let tokens = lexer::run(&data);
-    if debug {
-        println!("Generated tokens: {:?}", tokens);
-    }
-    match parser::parse(tokens) {
-        Ok(ast) => {
-            let args = parser::read_line(None, &mut lexer::run(&format!("[{}]", args)).iter().peekable(), &vec![&lexer::Expression::Semicolon]).unwrap();
-
-            if debug {
-                println!("{:?}", &ast);
-                println!("{:?}", args);
-            }
-
-            interpreter::run(ast, args)
-        }
-        Err(e) => println!("{}", e),
-    }
+    println!("{:?}", *parser::parse(&data));
 }
 
 pub fn version() -> &'static str {
--- test.em
+++ test.em
@@ -1,15 +1,6 @@
-fn main(args) {	
-	input = readln("Enter numbers to add up, or 'n' to stop: ");
-	result = 0;
-	while input != "n" {
-    println("Got number: " + input);
-	  if (number(input) == null) {
-			println("That wasn't a number");
-		}else {
-			result = result + number(input);	
-		}
-		input = readln("> ");
-	}
-
-	println("All those added up are " + result);
+fn main(name, age, race) {
+	main("name", "age", 10);
+	//balls = "Hello there young wizard";
 }
+
+
